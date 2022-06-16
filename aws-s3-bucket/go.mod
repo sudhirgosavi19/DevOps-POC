75afd7817ea5ae492bb15eab09abfa5543ceeadf
Sudhir Gosavi <sudhirgosavi@Sudhirs-MacBook-Air.local>
2022-06-16 17:03:13 +0530

    s3 bucket poc files

---
+++ aws-s3-bucket/go.mod
@@ -0,0 +1,7 @@
+module github.com/sudhirgosavi19/DevOps-POC
+
+go 1.18
+
+require github.com/aws/aws-sdk-go v1.44.33
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ aws-s3-bucket/s3_list-objects.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// Lists the items in the specified S3 Bucket
+//
+// Usage:
+//    go run s3_list_objects.go BUCKET_NAME
+func main() {
+	if len(os.Args) != 2 {
+		exitErrorf("Bucket name required\nUsage: %s bucket_name",
+			os.Args[0])
+	}
+
+	bucket := os.Args[1]
+
+	// Initialize a session in us-west-2 that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-south-1")},
+	)
+
+	// Create S3 service client
+	svc := s3.New(sess)
+
+	// Get the list of items
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
+	if err != nil {
+		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
+	}
+
+	for _, item := range resp.Contents {
+		fmt.Println("Name:         ", *item.Key)
+		fmt.Println("Last modified:", *item.LastModified)
+		fmt.Println("Size:         ", *item.Size)
+		fmt.Println("Storage class:", *item.StorageClass)
+		fmt.Println("")
+	}
+
+	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
+	fmt.Println("")
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}

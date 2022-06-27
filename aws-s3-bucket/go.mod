564d5d7f51e6c8db0947ad56127ff52d3d3a6b48
sudhirgosavi19 <sudhir.gosavi@dezerv.in>
2022-06-27 13:45:41 +0530

    file updated

--- aws-s3-bucket/go.mod
+++ aws-s3-bucket/go.mod
@@ -2,11 +2,49 @@ module github.com/sudhirgosavi19/DevOps-POC
 
 go 1.18
 
-require github.com/aws/aws-sdk-go v1.44.33
-
-require github.com/aws/smithy-go v1.11.3 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.44.33
+	github.com/dezerv/kit v0.0.0-20220621064322-bbf3f8b17abf
+)
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.16.5
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.13.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.10.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.4 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.2.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.11.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.24.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.9.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.14.0 // indirect
+	github.com/aws/smithy-go v1.11.3 // indirect
+	github.com/banzaicloud/logrus-runtime-formatter v0.0.0-20190729070250-5ae5475bae5e // indirect
+	github.com/mroth/weightedrand v0.4.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	go.mongodb.org/mongo-driver v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.5 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.4.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ses v1.5.2 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.10.0+incompatible // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- aws-s3-bucket/s3_list-objects.go
+++ aws-s3-bucket/s3_list-objects.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
+	"encoding/base64"
+	"encoding/csv"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/dezerv/kit/dto"
+
+	"github.com/dezerv/kit/email"
+	dezervaws "github.com/dezerv/kit/rpc/aws"
 )
 
 // Lists the items in the specified S3 Bucket
 //
 // Usage:
 //    go run s3_list_objects.go BUCKET_NAME
+func sum(array []float64) float64 {
+	result := 0.0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
 
 func main() {
 	currentTime := time.Now()
-	currentTime.Format("2006-01-02")
 	fmt.Println("YYYY-MM-DD : ", currentTime.Format("2006-01-02"))
 
 	if len(os.Args) != 2 {
@@ -25,33 +44,147 @@ func main() {
 			os.Args[0])
 	}
 
+	// credentials from the shared credentials file ~/.aws/credentials.
 	bucket := os.Args[1]
 
 	// Initialize a session in us-west-2 that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials.
-	sess, err := session.NewSession(&aws.Config{
+	sess, err1 := session.NewSession(&aws.Config{
 		Region: aws.String("ap-south-1")},
 	)
-
-	// Create S3 service client
+	if err1 != nil {
+		exitErrorf("Unable to access AWS account %v", err1)
+	}
 	svc := s3.New(sess)
-
-	// Get the list of items
+	s3Service := dezervaws.NewS3()
 	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
 	if err != nil {
 		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
 	}
-
+	var attachmentErr error = nil
+	ad := 0
+	attachments := []dto.EmailAttachment{}
+	var d_costArr []float64
+	var m_costArr []float64
 	for _, item := range resp.Contents {
-		fmt.Println("Name:         ", *item.Key)
-		//fmt.Println("Last modified:", *item.LastModified)
-		//fmt.Println("Size:         ", *item.Size)
-		//fmt.Println("Storage class:", *item.StorageClass)
-		//fmt.Println("")
+		//fmt.Println("Name:         ", *item.Key)
+		modDate := *item.LastModified
+		if currentTime.Format("2006-01-02") != modDate.Format("2006-01-02") {
+			continue
+		}
+		if filepath.Ext(*item.Key) != ".gz" {
+			continue
+		}
+		fmt.Println("file name : ", *item.Key)
+		ad++
+		var tempFilePath *os.File
+
+		fname := "file-" + fmt.Sprint(ad) + "-" + currentTime.Format("2006-01-02") + ".csv.gz"
+		tempFilePath, _ = ioutil.TempFile("", fname)
+		defer os.Remove(tempFilePath.Name())
+		err = s3Service.DownloadFileWithBucketParameter(*item.Key, tempFilePath.Name(), bucket)
+		if err != nil {
+			attachmentErr = fmt.Errorf("error reading file from s3::%s", err)
+			return
+		}
+		gzFile, err := os.Open(tempFilePath.Name())
+		if err != nil {
+			attachmentErr = fmt.Errorf("error opening pdf file %s", err)
+			return
+		}
+		defer gzFile.Close()
+		reader := bufio.NewReader(gzFile)
+		b, err := ioutil.ReadAll(reader)
+		if err != nil {
+			attachmentErr = fmt.Errorf("error reading the file %s", err)
+			return
+		}
+
+		content := base64.StdEncoding.EncodeToString(b)
+
+		fmt.Println("File name : ", fname)
+		f, err := os.Open(tempFilePath.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		gr, err := gzip.NewReader(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer gr.Close()
+		cr := csv.NewReader(gr)
+		rec, err := cr.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i, record := range rec {
+			if i == 0 {
+				continue
+			}
+			matched, err := regexp.MatchString(currentTime.Format("2006-01-02"), record[1])
+
+			if matched {
+				if record[9] != "Credit" {
+					//fmt.Println(record[9])
+					rec, err := strconv.ParseFloat(record[23], 32)
+					d_costArr = append(d_costArr, rec)
+					if err != nil {
+						fmt.Println("There is an error converting string to float.")
+					}
+				} else {
+					continue
+				}
+			} else if err != nil {
+				continue
+			}
+			if record[9] != "Credit" {
+				//fmt.Println(record[9])
+				rec1, err := strconv.ParseFloat(record[23], 32)
+				m_costArr = append(m_costArr, rec1)
+				if err != nil {
+					fmt.Println("There is an error converting string to float.")
+				}
+			} else {
+				continue
+			}
+
+		}
+
+		attachment1 := dto.EmailAttachment{
+			Content:     content,
+			Type:        "application/gzip",
+			Name:        fname,
+			Disposition: "attachment",
+		}
+		attachments = append(attachments, attachment1)
 	}
 
-	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
-	fmt.Println("")
+	d_cost := sum(d_costArr)
+	m_cost := sum(m_costArr)
+	s1 := fmt.Sprintf("%.2f", d_cost)
+	s2 := fmt.Sprintf("%.2f", m_cost)
+
+	econtent := "Hi All," + "\n" + "The AWS usage cost of the date: " + currentTime.Format("2006-01-02") + " is USD " + s1 + " and for the month, till date is USD " + s2
+
+	fmt.Println(attachmentErr)
+
+	emailReportFromEmail := "sudhir.gosavi@dezerv.in"
+	emailReportFromName := "Sudhir"
+	//emailReportFromEmail := os.Getenv("EMAIL_REPORT_FROM_EMAIL")
+	//emailReportFromName := os.Getenv("EMAIL_REPORT_FROM_NAME")
+	emailReportFromSubject := "AWS dashboard billing report" //os.Getenv("EMAIL_REPORT_FROM_SUBJECT")
+
+	from := dto.EmailAddress{Name: emailReportFromName, Email: emailReportFromEmail}
+	tos := []dto.EmailAddress{{Email: "sudhir.gosavi@dezerv.in"}, {Email: "yashu.gupta@dezerv.in"}}
+	cc := []dto.EmailAddress{{Email: "ankit@dezerv.in"}}
+	aclClient := email.NewAclClient("SjlHN1R3cXVaYmJuTy9FMElnL0xJSno0UXM4PQ")
+	emailContent := econtent
+	err = email.SendWithCC(aclClient, from, tos, nil, cc, &emailReportFromSubject, &emailContent, nil, attachments)
+
+	if err != nil {
+		exitErrorf("error sending email using acl: %s", err)
+	}
+
 }
 
 func exitErrorf(msg string, args ...interface{}) {
